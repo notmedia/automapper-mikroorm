37b5a7fb5f51b93d0f2c2068776c37300f172aa5
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-07-20 15:40:07 +0300

    Updated

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,23 @@
-import { Entity, Property, PrimaryKey } from '@mikro-orm/core';
+import {
+  Entity,
+  Property,
+  PrimaryKey,
+  OneToOne,
+  IdentifiedReference,
+  Reference,
+} from '@mikro-orm/core';
 import { AutoMap } from '@automapper/classes';
+
+@Entity({ tableName: 'book' })
+export class Book {
+  @PrimaryKey({ type: 'uuid', defaultRaw: 'uuid_generate_v4()' })
+  @AutoMap()
+  uuid!: string;
+
+  @Property()
+  @AutoMap()
+  name!: string;
+}
 
 @Entity({ tableName: 'users' })
 export class User {
@@ -15,8 +33,18 @@ export class User {
   @AutoMap()
   password!: string;
 
-  constructor(name: string, password: string) {
+  @OneToOne({
+    entity: () => Book,
+    owner: true,
+    eager: true,
+    fieldName: 'book_uuid',
+    wrappedReference: true,
+  })
+  book!: IdentifiedReference<Book>;
+
+  constructor(name: string, password: string, book: Book) {
     this.name = name;
     this.password = password;
+    this.book = Reference.create(book);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
-import { User } from './user.entity';
+import { Book, User } from './user.entity';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { UserProfile } from './user.profile';
 
 @Module({
-  imports: [MikroOrmModule.forFeature({ entities: [User] })],
+  imports: [MikroOrmModule.forFeature({ entities: [User, Book] })],
   providers: [UserProfile, UserService, UserResolver],
 })
 export class UserModule {}
